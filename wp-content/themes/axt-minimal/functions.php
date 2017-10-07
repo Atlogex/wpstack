1d43c80371cc2738e5e2794ec104433f8fa2ae61
Atlogex <atlogex@gmail.com>
2017-10-08 00:40:55 +0600

    Add theme Atlogex-minimal

---
+++ wp-content/themes/axt-minimal/functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Atlogex-Minimal functions and definitions
+ *
+ * @link https://atlogex.com/
+ * @link https://atlogex.com/linkforarticle
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
