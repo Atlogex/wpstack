dbed0109ad9d4aee2a923a7185716a688c2c4679
Atlogex <atlogex@gmail.com>
2017-12-07 23:59:34 +0600

    Add JS function number formatting

--- wp-content/themes/axt-func/assets.js
+++ wp-content/themes/axt-func/assets.js
@@ -2,8 +2,27 @@
 function vlidEmail(email) {
 	var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
 	var r = false;
-	if(pattern.test(email)){
+	if (pattern.test(email)) {
 		r = true;
 	}
 	return r;
+}
+
+// Formatted Number
+function formatStr(str) {
+	str = String(str);
+	str = str.replace(/(\.(.*))/g, '');
+	var arr = str.split('');
+	var str_temp = '';
+	if (str.length > 3) {
+		for (var i = arr.length - 1, j = 1; i >= 0; i--, j++) {
+			str_temp = arr[i] + str_temp;
+			if (j % 3 == 0) {
+				str_temp = ' ' + str_temp;
+			}
+		}
+		return str_temp;
+	} else {
+		return str;
+	}
 }
